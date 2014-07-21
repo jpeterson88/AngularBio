4a3a90bc030757fa18b07f9af88e20c17dc24293
Justin Peterson <jpeterson4223@yahoo.com>
2014-07-20 20:41:07 -0700

    add controllers, removed unused files

---
+++ Controllers/ContactController.js
@@ -0,0 +1,15 @@
+(function(){
+
+
+
+var app = angular.module('app',[]);
+app.controller('ContactController', ['$scope', function($scope){
+$scope.message = "derpderperpet";
+}]);
+
+
+$('#theCarousel').carousel();
+
+
+}());
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,1 @@
-(function(){
-
-
-
-var myApp = angular.module('myApp',[]);
-myApp.controller('MainController', ['$scope', function($scope){
-$scope.message = "Greeting is an act of communication in which human beings intentionally make their presence known to each other, to show attention to, and to suggest a type of relationship (usually cordial) or social status (formal or informal) between individuals or groups of people coming in contact with each other. Greetings sometimes are used just prior to a conversation or to greet in passing, such as on a sidewalk or trail. While greeting customs are highly culture and situation-specific and may change within a culture depending on social status and relationship, they exist in all known human cultures. Greetings can be expressed both audibly and physically, and often involve a combination of the two. This topic excludes military and ceremonial salutes but includes rituals other than gestures. A greeting can also be expressed in written communications, such as letters and emails.";
-}]);
-
-
-$('#theCarousel').carousel();
-
-
-
-
-}());
 
