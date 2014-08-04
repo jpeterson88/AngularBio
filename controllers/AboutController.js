3b163bcf07af5ebfd00c11ee381548921ed149a4
Justin Peterson <jpeterson4223@yahoo.com>
2014-08-03 21:09:39 -0700

    pruned unused files. added skeleton html files

---
+++ controllers/AboutController.js
@@ -0,0 +1,16 @@
+(function(){
+
+
+
+var app = angular.module('app',[]);
+app.controller('AboutController', ['$scope', function($scope){
+
+}]);
+
+
+$('#theCarousel').carousel();
+
+
+
+}());
+
---
+++ controllers/ContactController.js
@@ -0,0 +1,14 @@
+(function(){
+
+
+
+var app = angular.module('app',[]);
+app.controller('ContactController', ['$scope', function($scope){
+$scope.message = "derpderperpet";
+}]);
+
+
+
+
+
+}());
