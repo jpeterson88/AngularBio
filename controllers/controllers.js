27a549408d927c9a96d21eff8dfc7f9900599069
Justin Peterson <Sleepy@Justins-MacBook-Pro.local>
2014-08-30 09:58:59 -0700

    added functionality to software screen

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -57,48 +57,71 @@ bioApp.controller('SoftwareController', function($scope){
     $scope.githubUrl = 'https://github.com/sincerelysleepy';
 };
 
+
+ 	$scope.isActive = function isActive(path) {
+
+        return $location.path().substr(0, path.length) == path;
+    };
+
 	$scope.frameworks = [
 	{
 		frmwrkName: "C# / .NET",
-		location: "../templates/c-sharp.html"
+		location: "../templates/c-sharp.html",
+		description: "C# is the foundation of my experience with software development. I learned the majority of programming fundamamentals in the .NET world. I currently manage a CRM built on .NET technologies for one of the largest loan providers in the US. "
 	},
 		{
 		frmwrkName: "Javascript",
-		location: "../templates/javascript.html"
+		location: "../templates/javascript.html",
+		description: "Being relatively new to JS, I'm finding it syntactically similar to C#, which makes it easy to pick up. I've built this site in javascript and have been working with it a little at work creating a dashboard for one of our clients." 
 	},
 		{
 		frmwrkName: "WPF",
-		location: "../templates/wpf.html"
+		location: "../templates/wpf.html",
+		description: "Although I'm not a huge fan of WPF, I have quite a bit of experience working with XAML. My company's CRM is built with WPF which has helped me pick up the technology."
+	},
+		{
+		frmwrkName: "HTML",
+		location: "../templates/hypertext.html",
+		description: "Believe it or not, this site actually uses HTML. Shocker, I know! HTML had been popping up often for me as I'm transitioning from the .NET world to the open source web world."
+	},
+		{
+		frmwrkName: "ASP.NET MVC 5",
+		location: "../templates/asp.html",
+		description: "ASP.NET was my welcome to the web technology. I work for a consulting firm and we have a licensing page for a particular type of software which is built in ASP.NET. I've had the opportunity to dabble around, fix bugs and add certain features to this site."		
 	},
 		{
 		frmwrkName: "WCF",
-		location: "../templates/wcf.html"
+		location: "../templates/wcf.html",
+		description: "WCF is used quite a bit in the .NET world from what I've seen. I'm not necessarily a huge fan but I work with it daily as we have several APIs that are setup using WCF around our custom CRM that communicate with other services."
 	},
 		{
 		frmwrkName: "AngularJS",
-		location: "../templates/angular.html"
+		location: "../templates/angular.html",
+		description: "Angular has been my focus the past month and I've come to love it. Again, this site is built on Angular and is my first app I've built using it. Dynamic templates is one of the many great features in Angular and I'd highly recommend checking it out!"
 	},	
 		{
-		frmwrkName: "HTML",
-		location: "../templates/hypertext.html"
-	},
-		{
 		frmwrkName: "TSQL",
-		location: "../templates/tsql.html"
-	},
-		{
-		frmwrkName: "ASP.NET",
-		location: "../templates/asp.html"
+		location: "../templates/tsql.html",
+		description: "I use TSQL on a daily basis as that's our database of choice for my company's CRM application. I'm relatively well versed and am familiar with writing stored procs, basic CRUD, views, triggers and other concepts. I would eventually like to branch out and work with a NOSQL DB."
 	},
 		{
 		frmwrkName: "Bootstrap",
-		location: "../templates/bootstrap.html"
+		location: "../templates/bootstrap.html",
+		description: "As I'm not the best designer in the world, I find bootstrap a great framework to open up with for ideas. The more CSS I'm learning, the more daring I am becoming and eventually I will branch away from it, but for now, it's perfect. This site also uses bootstrap"
 	},
 		{
 		frmwrkName: "Misc",
-		location: "../templates/misc.html"
+		location: "../templates/misc.html",
+		description: "Along with the major technologies and frameworks listed above, I have learned some other frameworks and patterns which include but are not limited to Ideablade's DevForce, Miscrosoft's MEF, Caliburn micro, Telerik's UI controls, repository and factory patterns."
 	}
 	];
+
+	$scope.selectedFramework = $scope.frameworks[0];
+
+	$scope.setSelectedFramework = function(framework) {
+    $scope.selectedFramework = framework;
+};
+
 });
 
 
